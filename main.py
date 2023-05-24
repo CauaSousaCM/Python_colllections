4b8ba246434c6f2dd9a3b0e30ffb661e0a9576ea
CauaSousaCM <125994651+DevsQA@users.noreply.github.com>
2023-05-24 16:55:51 -0300

    update repository

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from Conta import Conta
+from Conta import (Conta, ContaCorrente, ContaPoupanca)
 
-print(Conta(88))
+conta1 = ContaCorrente(16)
+conta1.deposita(1000)
+conta1.passa_o_mes()
+print(conta1)
+
+conta2 = ContaPoupanca(17)
+conta2.deposita(1000)
+conta2.passa_o_mes()
+print(conta2)
